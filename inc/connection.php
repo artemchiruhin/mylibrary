33f045cfb0971d13a9bc0b3c21b0a6ba1ec456f3
Artem <69428285+artemchiruhin@users.noreply.github.com>
2021-09-16 11:09:46 +0500

    Updated

--- inc/connection.php
+++ inc/connection.php
@@ -1,12 +1,7 @@
 <?php
-    $host = "localhost";
-    $user = "id16467839_artem09521";
-    $pass = "KUW\(!(>%v}b5M&Z";
-    $db = "id16467839_mylibrary";
-
-    $host = "localhost";
-    $user = "mysql";
-    $pass = "mysql";
-    $db = "mylibrary";
+    $host = "host";
+    $user = "user";
+    $pass = "password";
+    $db = "dbname";
 
     $connect = mysqli_connect($host, $user, $pass, $db) or die("Ошибка" . mysqli_connect_error($connect));
